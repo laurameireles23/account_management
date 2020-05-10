2398f39539ad2715b2dec26a2b5c90c802ae8fbd
laurameireles23 <laura.meireles1@gmail.com>
2020-05-09 21:07:23 -0300

    Rename column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200508194052) do
+ActiveRecord::Schema.define(version: 20200509222431) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "account"
+    t.integer "number"
     t.integer "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
